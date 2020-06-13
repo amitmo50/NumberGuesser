38ff205a59cffe84d751febb04c2e9fc87020ee9
Amit Mor <amitmo50@gmail.com>
2020-06-14 00:56:04 +0300

    Set changes in reload button for play again in winning or losing situation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const game = document.querySelector('.game');
+const game = document.querySelector('#game');
 // get boundries of guessing
 const maxNumber = document.querySelector(".max-num");
 const minNumber = document.querySelector(".min-num");
@@ -14,40 +14,46 @@ const guessNumber = document.querySelector('#guess-input');
 let min = 1,
     max = 10,
     numberOfTry = 3;
-    winningNumber = Math.floor(Math.random() * max + 1);
+    winningNumber = 4;//Math.floor(Math.random() * max + 1);
 
 maxNumber.textContent = 10;
 minNumber.textContent = 1;
 
+game.addEventListener('mousedown', function(e){
+  if(e.target.className === "play-again"){
+    window.location.reload();
+  }
+});
+
 // Listen for a guess
-submitBtn.addEventListener('click', function(){
+submitBtn.addEventListener('click', playGame);
+
+// play game flow
+function playGame(){
   let guess = parseInt(guessNumber.value);
   if(isNaN(guess) || guess < min || guess > max){
      setMessage(`Please enter a nuber between ${min} and ${max}`, 'red');
   }else{
     if(guess === winningNumber){
       guessNumber.disabled = true;
-      submitBtn.disabled = true;
       setMessage(`Congratulations!, You\'ve guess the right the number is ${winningNumber}`, 'green');
       guessNumber.style.borderColor = 'green';
+      submitBtn.value = "PLAY AGAIN";
+      submitBtn.className += "play-again"; 
     }else{
       numberOfTry -= 1;
       setMessage(`${guess} is Wrong, try again you have ${numberOfTry} guess left`, 'black');
     }
     if(numberOfTry == 0){
       guessNumber.disabled = true;
-      submitBtn.disabled = true;
       setMessage(`Game Over!, You have ${numberOfTry} guess left please start a new game`, 'red');
+      submitBtn.value = "PLAY AGAIN";
+      submitBtn.className += "play-again"; 
     } 
   }
-   
-});
+}
 
-clearBtn.addEventListener('click', function(){
-  location.reload();
-});
-
-
+// set user information message
 function setMessage(messageValue, color){
   message.style.color = color;
   message.textContent = messageValue;
